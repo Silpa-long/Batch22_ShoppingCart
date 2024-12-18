9a35d6cb82f5f008503cfcea52cb8cfd3a3b23e4
palla123 <pallapusilpa@gmail.com>
2024-12-18 15:44:31 -0500

    Pushing Project for the firstTime

---
+++ ShoppingCartRealTimeProject/ShoppingCartRealTimeProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShoppingCartRealTimeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
